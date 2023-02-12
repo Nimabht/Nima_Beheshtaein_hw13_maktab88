4083ea563f3d91c0d46b4e9069c67703f2678400
nimabht <nimabeheshtaein99@gmail.com>
2023-02-13 01:31:43 +0330

    1-4 solved

---
+++ 1-4/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+const homeController = (_req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+};
+
+const imageController = (_req, res) => {
+  res.sendFile(`${__dirname}/views/img.jpg`);
+};
+
+const notFoundController = (req, res) => {
+  res.status(404).send("Sorry, that route is not found.");
+};
+
+app.get("/home", homeController);
+app.get("/picture", imageController);
+
+app.use(notFoundController);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}...`);
+});
