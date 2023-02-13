2f5eaa59c9325ccf3a379b4a3a1c12f07680faba
nimabht <nimabeheshtaein99@gmail.com>
2023-02-13 15:05:57 +0330

    1-6 solved

---
+++ 1-6/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+const pageController = (req, res) => {
+  const noPage = req.params.no;
+  res.sendFile(`${__dirname}/views/index${noPage}.html`);
+};
+
+const notFoundController = (_req, res) => {
+  res.status(404).send("Sorry, that route is not found.");
+};
+
+app.get("/page:no", pageController);
+
+app.use(notFoundController);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}...`);
+});
